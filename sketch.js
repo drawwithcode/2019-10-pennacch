13ce491d0f149e5007415847d6f5acc184545d2a
pennacch <55466516+pennacch@users.noreply.github.com>
2019-12-10 16:49:49 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,59 @@
-function preload(){
-  // put preload code here
-}
+var inc = 0;
+var incRot = 0;
 
 function setup() {
-  // put setup code here
+
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  document.documentElement.style.overflow = 'hidden';
+
+  angleMode(DEGREES);
+  frameRate(12);
+  angleMode(DEGREES);
+
+  noStroke();
+
 }
 
 function draw() {
-  // put drawing code here
+
+
+  var from = color(200, 0, 0);
+  var to = color(0, 0, 100);
+  background(0);
+
+  ortho();
+
+  camera(width / 2, -height / 2, 100, 0, 0, 0, 0, 1, 0);
+  directionalLight(255, 255, 255, 0, 1, -1);
+  ambientLight(190);
+
+  rotateY(frameCount * 3);
+  for (i = -5; i < 5; i++) {
+    push();
+    translate(0, 0, -i * 40);
+    for (j = -5; j < 5; j++) {
+      push();
+
+      inc += 0.9;
+      var max = dist(mouseX, mouseY, pmouseX, pmouseY);
+      var h = noise(inc) * (100 + max);
+
+
+
+      var hCol = map(h, 10, 200, 0, 1);
+      fill(lerpColor(from, to, hCol));
+
+      translate(-j * 40, 0, 0);
+
+
+      box(30, h, 30);
+      pop();
+    }
+
+    pop();
+
+  }
+
+
+
 }
